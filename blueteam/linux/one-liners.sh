50faccea61b9212de8e4177e99eb0746eedc8e39
Dack985 <151111647+Dack985@users.noreply.github.com>
2025-02-23 17:58:44 -0500

    Create one-liners.sh

---
+++ blueteam/linux/one-liners.sh
@@ -0,0 +1,19 @@
+#detect all files changed within the last 10 mins
+
+
+#show every users crontab
+
+#show every users ssh private key file
+
+#update pam
+
+#check for binaries over 2mb
+
+
+#check ssh attempts
+
+#show every running service
+
+#basic rootkit search
+
+#show mounted services
