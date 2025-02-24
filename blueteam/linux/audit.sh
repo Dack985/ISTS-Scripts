764517bb66247c132d0dcd94879aa5ec992de9ae
Dack985 <151111647+Dack985@users.noreply.github.com>
2025-02-24 10:28:48 -0500

    Update audit.sh

--- blueteam/linux/audit.sh
+++ blueteam/linux/audit.sh
@@ -1,1 +1,6 @@
+echo"showing all users with shell access"
+awk -F: '{print $1, $7}' /etc/passwd | grep -v nologin
 
+echo "listing all users with elevated priveldegs"
+
+echo "showing the contents of /etc/sudoers to see if any users have sudo privs"
