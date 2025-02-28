98d23f9e21f02209538db30a6d65f933c78e918f
Dack985 <151111647+Dack985@users.noreply.github.com>
2025-02-27 19:19:36 -0500

    Update audit.sh

--- blueteam/linux/audit.sh
+++ blueteam/linux/audit.sh
@@ -1,6 +1,20 @@
 echo"showing all users with shell access"
 awk -F: '{print $1, $7}' /etc/passwd | grep -v nologin
 
-echo "listing all users with elevated priveldegs"
+
+echo "list all users with root uid"
+
+grep 'x:0:' /etc/passwd
+# or you can use cat /etc/passwd | cut -f1,3,4 -d":" | grep"0:0" | cut -f1 -d":" | awk '{print $1}'
+echo "listing all users within the root group"
+
+grep -E '^(root|wheel|adm|admin):' /etc/group
 
 echo "showing the contents of /etc/sudoers to see if any users have sudo privs"
+sudo grep -E '\sALL[=(]' /etc/sudoers
+
+
+echo"list all programs that have a SUID bit that allows the program to be executed as root."
+sudo find / -perm -04000
+
+
