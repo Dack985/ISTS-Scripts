38708ca414fa73664cec1c06d5f67bad3d354b34
Nick <73262982+nicholasc0523@users.noreply.github.com>
2025-03-06 01:22:51 -0500

    Update iron-dome.sh

--- blueteam/linux/iron-dome.sh
+++ blueteam/linux/iron-dome.sh
@@ -7,12 +7,32 @@ if [ $EUID -ne 0 ]; then
 fi
 
 
-install_packages () {
-  if dpkg -l | grep -q "^ii  iptables-persistent "; then
-    echo "All necessary packages are already installed."
+scoring_engine_ip() {
+  echo "Please enter the IP address of the scoring engine."
+  read -r ip
+  if [[ "$ip" =~ ^([0-9]{1,3}\.){3}[0-9]{1,3}$ ]]; then
+    IFS='.' read -r -a octets <<< "$ip"
+    for octet in "${octets[@]}"; do
+      if [ "$octect" -lt 0 ] || [ "$octet" -gt 255 ]; then
+        echo "Invalid IP: each octet must be between 0 and 255."
+	return 1
+      fi
+    done
+    scoring="$ip"
+    echo "Scoring-engine IP: $scoring"
   else
-    echo "You don't have all necessary packages. Installing now..."
+    echo "Invalid IP format. The input IP must be in the format: X.X.X.X"
+    return 1
+  fi
+}
+
+install_packages () {
+  #if dpkg -l | grep -q "^ii  iptables-persistent "; then
+  if ! command -v iptables-save &> /dev/null; then
+    echo "You don't have iptables-persistent. Installing..."
     apt install -y iptables-persistent
+  else
+    echo "Hey, iptables-persistent is installed--as you were!"
   fi
 }
 
@@ -22,7 +42,7 @@ iptables_ruleset () {
   iptables -A OUTPUT -j ACCEPT
   # Chain the default chain rules to deny any-any
   iptables -P INPUT -j DROP
-  #iptables -P FORWARD -j DROP
+  iptables -P FORWARD -j DROP
   iptables -P OUTPUT -j DROP
   # Allow loopback interface traffic in/out
   iptables -A INPUT -i lo -j ACCEPT
@@ -42,8 +62,17 @@ iptables_ruleset () {
   iptables -A OUTPUT -p tcp --dport 80 -m conntrack --ctstate NEW -j ACCEPT
   iptables -A OUTPUT -p tcp --dport 443 -m conntrack --ctstate NEW -j ACCEPT
   # Add the scoring-engine IP to whitelist all traffic to and from that device
-  # iptables -A INPUT -s $scoring -m conntrack --ctstate NEW -j ACCEPT
-  # iptables -A OUTPUT -d $scoring -m conntrack --ctstate NEW -j ACCEPT
+  iptables -A INPUT -s $scoring -m conntrack --ctstate NEW -j ACCEPT
+  iptables -A OUTPUT -d $scoring -m conntrack --ctstate NEW -j ACCEPT
+}
+
+remove_training_wheels () {
+  iptables -D INPUT 1
+  iptables -D OUTPUT 1
+}
+
+flush_iptables () {
+  iptables -F
 }
 
 save_config () {
@@ -51,14 +80,23 @@ save_config () {
 }
 
 
+# Call the function to enter scoring IP
+while true; do
+  scoring_engine_ip
+  if [ -n "$scoring" ]; then
+    break
+  fi
+done
+
 # Menu selection
-echo -ne "Make a choice: \n1) Check/Install Packages\n2) Apply Basic Ruleset\n3) Save Configurations\n4) All"
+echo -ne "SETUP OPTIONS: \n1) Check For/Install Packages\n2) Apply Ruleset to Running Config\n3) Safe Setup\n4) Full Setup\n5) Un-Bork the Box"
 read -r choice
 
 case $choice in
   1) install_packages ;;
   2) iptables_ruleset ;;
-  3) save_config ;;
-  4) install_packages; iptables_ruleset; save_config ;;
-  *) install_packages ;;
+  3) install_packages; iptables_ruleset; save_config ;;
+  4) install_packages; iptables_ruleset; remove_training_wheels; save_config ;;
+  5) iptables_flush; save_config ;;
+  *) echo "Please choose an option next time." ;;
 esac
