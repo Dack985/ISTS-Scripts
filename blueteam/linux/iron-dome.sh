73ee50d06e4bad838e5f3de28f41ff58d9ed1751
Nick <73262982+nicholasc0523@users.noreply.github.com>
2025-03-07 12:53:54 -0500

    Cleaned up unused functions

--- blueteam/linux/iron-dome.sh
+++ blueteam/linux/iron-dome.sh
@@ -6,30 +6,24 @@ if [ $EUID -ne 0 ]; then
   exit 2
 fi
 
-echo "installing required packages... bozo... i am the firewall god "
-  if dpkg -l | grep -q "^ii  iptables-persistent "; then
-    echo "--> Nice, iptables-persistent is installed--moving on!"
-    sleep 0.4
-  else
-    echo "--> Missing iptables-persistent package. Installing now..."
-    apt install -y iptables-persistent
-  fi
+# Automatically check for and install packages if they are not there
+echo "Installing required packages... bozo... I am the firewall god!!!"
+if dpkg -l | grep -q "^ii  iptables-persistent "; then
+  echo "--> Nice, iptables-persistent is installed--moving on!"
+  sleep 0.4
+else
+  echo "--> Missing iptables-persistent package. Installing now..."
+  apt install -y iptables-persistent
+fi
 
-#  if ! command -v iptables-save &> /dev/null; then
-#    echo "You don't have iptables-persistent. Installing..."
-#    apt install -y iptables-persistent
-#  else
-#    echo "Hey, iptables-persistent is installed--as you were!"
-#  fi
-
-scoring_engine_ip() {
+scoring_engine_ip () {
   echo "Please enter the IP address of the scoring engine:"
   read -r ip
   if [[ "$ip" =~ ^([0-9]{1,3}\.){3}[0-9]{1,3}$ ]]; then
     IFS='.' read -r -a octets <<< "$ip"
     for octet in "${octets[@]}"; do
       if [ $((octet)) -lt 0 ] || [ $((octet)) -gt 255 ]; then
-	echo "err: Invalid IP. Each octet must be between 0 and 255."
+	echo "### Invalid IP. Each octet must be between 0 and 255."
 	return 1
       fi
     done
@@ -37,12 +31,12 @@ scoring_engine_ip() {
     scoring="$ip"
     echo ""
   else
-    echo "err: Invalid IP. Must input in the format: X.X.X.X or ensure ip address is correct"
+    echo "### Invalid IP. Must input in the format: X.X.X.X or ensure ip address is correct"
     return 1
   fi
 }
 
-private_ip_addresses() {
+private_ip_addresses () {
   echo "Please enter the IP addresses of your private devices separated by spaces (eg 192.168.1.1 192.168.1.2):"
   read -a private_ips
 
@@ -51,29 +45,29 @@ private_ip_addresses() {
   for private_ip in "${private_ips[@]}"; do
     if [[ "$private_ip" =~ ^((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])\.){3}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])$ ]]; then
       valid_ips+=("$private_ip")
-      echo "--> valid private IP detected and added: $private_ip"
+      echo "--> Valid private IP detected and added: $private_ip"
     else
-      echo "err: Invalid IP. Must input in the format: X.X.X.X or ensure ip address is correct"
+      echo "### Invalid IP. Must input in the format: X.X.X.X or ensure ip address is correct"
     fi
   done
 
   if [ ${#valid_ips[@]} -gt 0 ]; then
     echo "Valid IPs collected: ${valid_ips[*]}"
   else
-    echo "No valid IPs were entered."
+    echo "### No valid IPs were entered."
   fi
   
   forgotten_private_ip
 }
 
-forgotten_private_ip() {
-    echo "Would you like to add any IP addresses you may have missed?"
-    select yn in "Yes" "No"; do
-        case $yn in
-        Yes ) private_ip_addresses; break;;
-        No ) return;;
-        esac
-    done
+forgotten_private_ip () {
+  echo "Would you like to add any IP addresses you may have missed?"
+  select yn in "Yes" "No"; do
+    case $yn in
+      Yes) private_ip_addresses; break;;
+      No) return;;
+    esac
+  done
 }
 
 iptables_ruleset () {
@@ -141,25 +135,23 @@ while true; do
 done
 
 # ~Documentation~
-# 1 - Check for iptables persistent package and install if not found
-# 2 - Apply our custom ruleset to the running config
-# 3 - Apply the custom ruleset and save it, but do not remove the safety allow any-any
-# 4 - Apply the custom ruleset, remove the safety allow any-any, and save the config
-# 5 - Set each chain rule to accept all traffic and flush the individual rules
-# 6 - Exit the program
+# 1 - Apply our custom ruleset to the running config
+# 2 - Apply the custom ruleset and save it, but do not remove the safety allow any-any
+# 3 - Apply the custom ruleset, remove the safety allow any-any, and save the config
+# 4 - Set each chain rule to accept all traffic and flush the individual rules
+# 5 - Exit the program
 
 
 #not added in yet as firewall rules/cases havent been added in to quantify using it, will augement firewall rules soon function (private_ip_addresses)
 # Menu selection starts 
-echo -ne "--FIREWALL CONFIGURATION-- \n1) Check For & Install Packages\n2) Quick Config\n3) Safe Setup\n4) Launch the Iron Dome\n5) Unbork the Box\n6) Exit\n"
+echo -ne "--FIREWALL CONFIGURATION-- \n1) Quick Config\n2) Safe Setup\n3) Launch the IRON DOME\n4) Unbork the Box\n5) Exit\n"
 read -r choice
 
 case $choice in
-  1) install_packages ;;
-  2) iptables_ruleset ;;
-  3) install_packages; iptables_ruleset; save_config ;;
-  4) install_packages; iptables_ruleset; remove_training_wheels; save_config ;;
-  5) iptables_reset; save_config ;;
-  6) exit ;;
+  1) iptables_ruleset ;;
+  2) iptables_ruleset; save_config ;;
+  3) iptables_ruleset; remove_training_wheels; save_config ;;
+  4) iptables_reset; save_config ;;
+  5) exit ;;
   *) echo "--> Defaulting to exit."; exit ;;
 esac
